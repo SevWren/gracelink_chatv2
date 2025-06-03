49f44de787d2e0495b12dfe86cb486f3f7bf4b68
Mike M <mmueller07@gmail.com>
2025-06-02 20:59:25 -0500

    base: '/gracelink_chatv2/', // IMPORTANT: Replace 'gracelink_chat' with your GitHub repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gracelink_chat/', // IMPORTANT: Replace 'gracelink_chat' with your GitHub repository name
+  base: '/gracelink_chatv2/', // IMPORTANT: Replace 'gracelink_chat' with your GitHub repository name
   build: {
     outDir: 'dist',
   }
